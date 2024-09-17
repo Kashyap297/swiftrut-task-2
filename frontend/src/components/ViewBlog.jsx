f625b189ce09d55fdc1c37b408a13dcba6a786e1
Kashyap297 <kashyap29700@gmail.com>
2024-09-18 04:18:19 +0530

    make some changes in that

--- frontend/src/components/ViewBlog.jsx
+++ frontend/src/components/ViewBlog.jsx
@@ -22,6 +22,18 @@ const ViewBlog = () => {
   };
 
   if (!post) return <div>Loading...</div>;
+
+  const formattedDate = new Date(post.createdAt).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const formattedTime = new Date(post.createdAt).toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
   return (
     <>
       <div class="relative p-4">
@@ -29,7 +41,12 @@ const ViewBlog = () => {
           <div class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
             <div class="mt-48">
               <h3 class="text-2xl font-bold my-5">{post.title}</h3>
+
               <p class="text-base leading-8 my-5">{post.content}</p>
+              <div className="flex justify-between items-center mb-5">
+                <span className="text-gray-500">Posted on {formattedDate}</span>
+                <span className="text-gray-500">{formattedTime}</span>
+              </div>
               <button
                 onClick={handleDelete}
                 className="bg-red-500 text-white px-4 py-2 bg-gradient-to-r from-red-500 to-orange-600 rounded-lg"
