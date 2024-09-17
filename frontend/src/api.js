5355c4777f4ee34cdaf74dce026cf4278676548a
Kashyap297 <kashyap29700@gmail.com>
2024-09-18 05:16:17 +0530

    vercel backend live update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // Change this if you deployed the API
+  baseURL: "https://swiftrut-task-2-nine.vercel.app/api",
 });
 
 export const getAllPosts = () => api.get("/posts");
